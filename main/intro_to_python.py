b564c035f501b7b32647fa982fa430e5ee9630f9
saturn811 <122565491+saturn811@users.noreply.github.com>
2023-01-20 09:49:47 -0500

    Add files via upload

--- main/intro_to_python.py
+++ main/intro_to_python.py
@@ -1,1 +1,24 @@
+import numpy as np
 
+
+a = np.array([[1,0,0],[0,1,0],[0,0,1]])
+
+for line in a:
+    print ('  '.join(map(str, line)))
+    
+print("\n")
+
+
+a = np.array([[1,3,3],[3,1,3],[3,3,1]])
+
+for line in a:
+    print ('  '.join(map(str, line)))
+
+print("\n")
+
+a = np.array([[1,3],[3,1],[3,3]])
+
+for line in a:
+    print ('  '.join(map(str, line)))
+
+    
